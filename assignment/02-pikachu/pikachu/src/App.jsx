fb2f74e81eb2453841f2cc9cb305e21c8ab98386
Minsoo Cho <textign@gmail.com>
2024-05-16 20:50:28 +0900

    pikachu

---
+++ assignment/02-pikachu/pikachu/src/App.jsx
@@ -0,0 +1,13 @@
+import Pikachu from "./componets/Pikachu";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Pikachu />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ assignment/02-pikachu/pikachu/src/componets/Pikachu/Pikachu.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import "./Pikachu.css";
+
+function Pikachu() {
+  const [positionX, setPositionX] = useState(0);
+  const [positionY, setPositionY] = useState(0);
+  const [jump, setJump] = useState("");
+
+  const moveDown = () => {
+    setPositionY((prev) => {
+      if (prev > 800) {
+        return 900;
+      }
+      return prev + 100;
+    });
+  };
+  const moveUp = () => {
+    setPositionY((prev) => {
+      if (prev < 100) {
+        return 0;
+      } else {
+        return prev - 100;
+      }
+    });
+  };
+  const moveLeft = () => {
+    setPositionX((prev) => {
+      if (prev < 100) {
+        return 0;
+      }
+      return prev - 100;
+    });
+  };
+  const moveRight = () => {
+    setPositionX((prev) => {
+      if (prev > 800) {
+        return 900;
+      }
+      return prev + 100;
+    });
+  };
+  const jumpUp = () => {
+    const value = "jumpUp 0.3s ease";
+    setJump(value);
+  };
+  const jumpDown = () => {
+    const value = "jumpDown 0.3s ease";
+    setJump(value);
+  };
+
+  const handleKeyPress = (e) => {
+    const keyName = e.key;
+    switch (keyName) {
+      case "ArrowDown":
+        moveDown();
+        break;
+      case "ArrowUp":
+        moveUp();
+        break;
+      case "ArrowLeft":
+        moveLeft();
+        break;
+      case "ArrowRight":
+        moveRight();
+        break;
+      case " " || "Spacebar":
+        jumpUp();
+        break;
+    }
+  };
+
+  const handleKeyPressOut = (e) => {
+    if (e.key === "Spacebar" || e.key === " ") {
+      jumpDown();
+    }
+  };
+  return (
+    <div id="board">
+      <input
+        className="pikachu"
+        onKeyDown={handleKeyPress}
+        onKeyUp={handleKeyPressOut}
+        autoFocus
+        style={{
+          transform: `translate(${positionX}%,${positionY}%)`,
+          animation: `${jump}`,
+        }}
+      />
+    </div>
+  );
+}
+export default Pikachu;
